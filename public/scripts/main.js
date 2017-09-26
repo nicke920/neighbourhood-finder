b0680af0ed00eeca1df429014edcebc60190a90a
Nick Evans <evans2nick@gmail.com>
2017-09-25 21:28:36 -0400

    user can enter an address in input and input will geocode address to get latlng

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -18,6 +18,8 @@ $(function () {
 
 	var geocoder = new google.maps.Geocoder();
 
+	var locationObject;
+
 	function codeAddress() {
 		console.log('fucntion fired');
 		var address = $('#address').val();
@@ -28,6 +30,26 @@ $(function () {
 			console.log(status);
 			if (status == 'OK') {
 				map.setCenter(results[0].geometry.location);
+				locationObject = results[0].geometry.location;
+				console.log('87asdf', locationObject);
+				listPlaces(locationObject);
+			}
+		});
+	}
+
+	function listPlaces(location) {
+		var request = {
+			location: location,
+			radius: 10000,
+			type: ['restaurant']
+		};
+
+		var service;
+		service = new google.maps.places.PlacesService(map);
+		service.nearbySearch(request, function (results, status) {
+			console.log('nigga', results);
+			if (status == 'OK') {
+				console.log('okayed');
 			}
 		});
 	}
