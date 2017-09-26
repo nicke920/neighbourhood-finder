69cce424a7eb88f2aa19a7d1f09a0e673ff4fa92
Nick Evans <evans2nick@gmail.com>
2017-09-25 21:11:36 -0400

    implemented map and created search bar

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,3 +1,38 @@
 $(function()  {	
+	console.log('inist')
+	var map;
+
+	var service;
+
+	var center;
+
+	center = {lat: 49.283103, lng: -123.119290};
+
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: center, 
+		zoom: 11
+	})
+
+	var geocoder = new google.maps.Geocoder()
+
+
+	function codeAddress() {
+		console.log('fucntion fired')
+		var address = $('#address').val();
+		geocoder.geocode({
+			address: address
+		}, function(results, status) {
+				console.log(results)
+				console.log(status)
+			if (status == 'OK') {
+				map.setCenter(results[0].geometry.location)
+			}
+		})
+	}
+
+
+	$('#gotime').on('click', function() {
+		codeAddress()
+	})
 
 })
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,6 +1,40 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-"use strict";
+'use strict';
 
-$(function () {});
+$(function () {
+	console.log('inist');
+	var map;
+
+	var service;
+
+	var center;
+
+	center = { lat: 49.283103, lng: -123.119290 };
+
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: center,
+		zoom: 11
+	});
+
+	var geocoder = new google.maps.Geocoder();
+
+	function codeAddress() {
+		console.log('fucntion fired');
+		var address = $('#address').val();
+		geocoder.geocode({
+			address: address
+		}, function (results, status) {
+			console.log(results);
+			console.log(status);
+			if (status == 'OK') {
+				map.setCenter(results[0].geometry.location);
+			}
+		});
+	}
+
+	$('#gotime').on('click', function () {
+		codeAddress();
+	});
+});
 
 },{}]},{},[1]);
