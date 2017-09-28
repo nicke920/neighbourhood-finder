450a6f285b765bbac75ce1d91396d49444144bf6
Nick Evans <evans2nick@gmail.com>
2017-09-27 21:32:17 -0400

    added function to allow user to search for multiple categories and list them

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -23,8 +23,8 @@ $(function()  {
 	function codeAddress() {
 		var address = $('#address').val();
 		var rad = $('#radius').val();
+		//convert radius into number, since it defaults to a string
 		rad = parseInt(rad)
-
 
 		geocoder.geocode({
 			address: address
@@ -43,10 +43,9 @@ $(function()  {
 	}
 
 	function listPlaces(location, radius) {
-		
 		var request = {
 			location: location, 
-			rankby: 'distance', 
+			rankby: 'prominence', 
 			type: ['restaurant'],
 			radius: radius
 		}
@@ -55,7 +54,32 @@ $(function()  {
 		service = new google.maps.places.PlacesService(map);
 		service.nearbySearch(request, function(results, status) {
 			if (status == 'OK') {
-				getMarkers(results)
+				console.log('cuz',results)
+				getMarkers(results, 'res')
+
+				$.each(results, function(index, place) {
+					$('#list').append($('<div>').append(`<li>${place.name}</li>`))
+				})
+			}
+		})
+
+		var request1 = {
+			location: location, 
+			rankby: 'prominence', 
+			type: ['bank'],
+			radius: radius
+		}
+
+		var service1;
+		service1 = new google.maps.places.PlacesService(map);
+		service1.nearbySearch(request1, function(results, status) {
+			if (status == 'OK') {
+				console.log('cuz',results)
+				getMarkers(results, 'ban')
+
+				$.each(results, function(index, place) {
+					$('#list2').append($('<div>').append(`<li>${place.name}</li>`))
+				})
 			}
 		})
 	}
@@ -77,11 +101,75 @@ $(function()  {
 
 		circle.bindTo('center', marker, 'position')
 
+		//center map on center marker
 		map.setCenter(marker.position)
+
 		var zoomin = radiusToZoom(radius)
 		
+		//automatically zoom map to fit the radius of the circle overlay
 		map.setZoom(zoomin)
 	}
+
+
+
+	function getMarkers(arrayOfPlaces, icon) {
+
+		$.each(arrayOfPlaces, function(index, place) {
+			var placeName = place.name;
+			var placeCoords = place.geometry.location;
+			var placeAddress = place.vicinity;
+			var placeid = place.place_id;
+
+			var iconType;
+
+			if (icon === 'ban') {
+				iconType = '../../assets/money-bag.png'
+			} else {
+				iconType = '../../assets/pin.png'
+			}
+			var marker = new google.maps.Marker({
+				position: placeCoords, 
+				address: placeAddress,
+				name: placeName,
+				map: map, 
+				id: placeid,
+				animation: google.maps.Animation.DROP,
+				icon: iconType
+			})
+
+
+			markers.push(marker)
+
+			marker.addListener('click', function() {
+
+				var deets = this;
+
+				var request = {
+					placeId: deets.id
+				}
+
+				var service;
+				service = new google.maps.places.PlacesService(map);
+				service.getDetails(request, function(results, status) {
+
+					if (status == google.maps.places.PlacesServiceStatus.OK) {
+						$('h1#name').text(results.name)
+					}
+				})
+			})
+		})
+	}
+	
+
+	$('#gotime').on('click', function() {
+		codeAddress()
+	})
+
+
+
+
+
+
 
 	function radiusToZoom( r ){
 	    var w = $('#map').width();
@@ -96,55 +184,5 @@ $(function()  {
 	    }
 	    return z;
 	}
-
-	function getMarkers(arrayOfPlaces) {
-		var bounds = new google.maps.LatLngBounds();
-
-		$.each(arrayOfPlaces, function(index, place) {
-			var placeName = place.name;
-			var placeCoords = place.geometry.location;
-			var placeAddress = place.vicinity;
-			var placeid = place.place_id;
-
-			bounds.extend(placeCoords)
-
-			var marker = new google.maps.Marker({
-				position: placeCoords, 
-				address: placeAddress,
-				name: placeName,
-				map: map, 
-				id: placeid,
-				animation: google.maps.Animation.DROP
-			})
-
-			// map.fitBounds(bounds)
-
-			markers.push(marker)
-
-			marker.addListener('click', function() {
-				console.log('clicked')
-				var deets = this;
-
-				var request = {
-					placeId: deets.id
-				}
-
-				var service;
-				service = new google.maps.places.PlacesService(map);
-				service.getDetails(request, function(results, status) {
-					console.log('22', results)
-					if (status == google.maps.places.PlacesServiceStatus.OK) {
-						console.log('reqqqq', results.name)
-						$('h1#name').text(results.name)
-					}
-				})
-			})
-		})
-	}
-	
-
-	$('#gotime').on('click', function() {
-		codeAddress()
-	})
 
 })
